be4b1760c50a9f04a15e509f0a7e0d1eb56843ba
Fabian Damken <fabian.damken@prodyna.com>
2020-03-21 23:25:58 +0100

    Added 6.1

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -8,46 +8,36 @@ log = gym.logger
 log.set_level(gym.logger.INFO)
 
 ENV_NAME = 'FrozenLake-v0'
-RANDOM_STEPS = 100
 TEST_EPISODES = 20
 DISCOUNT_FACTOR = 0.9
+LEARNING_RATE = 0.2
 
 
 
 class Agent:
     def __init__(self, env_name):
-        #  { (source state, action) --> reward }
-        self.rewards = collections.defaultdict(float)
-        #  { (source state, action) --> { target state --> count } }
-        self.transitions = collections.defaultdict(collections.Counter)
-        #  { (state, action) --> value }
+        #  { (state, action) --> action value }
         self.action_values = collections.defaultdict(float)
 
         self.env = gym.make(env_name)
         self.state = self.env.reset()
 
 
-    def play_n_random_steps(self, count):
-        # Waiting to the end of an episode is not necessary!
-        for _ in range(count):
-            action = self.env.action_space.sample()
-            next_obs, reward, done, _ = self.env.step(action)
+    def sample_env(self):
+        action = self.env.action_space.sample()
+        obs, reward, done, _ = self.env.step(action)
 
-            self.rewards[(self.state, action, next_obs)] = reward
-            self.transitions[(self.state, action)][next_obs] += 1
+        old_state = self.state
+        self.state = self.env.reset() if done else obs
 
-            self.state = self.env.reset() if done else next_obs
+        return old_state, action, reward, obs
 
 
-    def q_iteration(self):
-        for state in range(self.env.observation_space.n):
-            for action in range(self.env.action_space.n):
-                dest_counts = self.transitions[(state, action)]
-                total = sum(dest_counts.values())
-                q = 0.0
-                for dest_state, count in dest_counts.items():
-                    q += (count / total) * (self.rewards[(state, action, dest_state)] + DISCOUNT_FACTOR * self.action_values[(dest_state, self.select_action(dest_state))])
-                self.action_values[(state, action)] = q
+    def bellman_update(self):
+        old_state, action, reward, new_state = self.sample_env()
+        new_q = reward + DISCOUNT_FACTOR * self.select_action(new_state)[0]
+        old_q = self.action_values[(old_state, action)]
+        self.action_values[(old_state, action)] = (1 - LEARNING_RATE) * old_q + LEARNING_RATE * new_q
 
 
     def select_action(self, state):
@@ -57,19 +47,16 @@ class Agent:
             if best_value is None or best_value < action_value:
                 best_value = action_value
                 best_action = action
-        return best_action
+        return best_value, best_action
 
 
     def play_episode(self, env):
         total_reward = 0.0
         obs = env.reset()
         while True:
-            action = self.select_action(obs)
+            _, action = self.select_action(obs)
             next_obs, reward, done, _ = env.step(action)
             total_reward += reward
-
-            self.rewards[(obs, action, next_obs)] = reward
-            self.transitions[(obs, action)][next_obs] += 1
 
             if done:
                 break
@@ -83,15 +70,15 @@ class Agent:
 if __name__ == "__main__":
     test_env = gym.make(ENV_NAME)
     agent = Agent(ENV_NAME)
-    writer = SummaryWriter(comment = '-frozenlake_q_iteration')
+    writer = SummaryWriter(comment = '-frozenlake_q_learning')
 
     iter_no = 0
     best_reward = 0.0
     while True:
         iter_no += 1
 
-        agent.play_n_random_steps(RANDOM_STEPS)
-        agent.q_iteration()
+        # Executing multiple bellman updates in one iteration indeed leads to less iterations.
+        agent.bellman_update()
 
         reward = 0.0
         for _ in range(TEST_EPISODES):
